175e33ccf6b939ff511661d6e4940507a117069e
theraushan17 <theraushan17@gmail.com>
2022-05-09 23:19:28 +0530

    my first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// // import * as serviceWorker from './serviceWorker';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//    <App/>
+//   </React.StrictMode>
+  
+  
+  
+    
+//   ,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
